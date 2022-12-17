94ab255c848e06a942a0dd74e660eb52788b9e24
neilukrida <neil.422021004@civitas.ukrida.ac.id>
2022-12-17 20:48:49 +0700

    Payment gateway

--- app/Http/Requests/User/Checkout/Store.php
+++ app/Http/Requests/User/Checkout/Store.php
@@ -29,9 +29,8 @@ class Store extends FormRequest
             'name' => 'required|string', /*kenapa string karena nanti data yang diterima menggunakan string*/
             'email' => 'required|email|unique:users,email,'.Auth::id().',id', /*ini jadi email yang digunakan untuk login tidak dikenal sebagai unique lagi tapi bisa digunakan*/
             'occupation' => 'required|string',
-            'card_number' => 'required|numeric|digits_between:8,16',
-            'expired' => 'required|date|date_format:Y-m|after_or_equal:' . $expiredValidation,
-            'cvc' => 'required|numeric|digits:3',
+            'phone' => 'required|string',
+            'address' => 'required|string',
         ];
     }
 }
